e1f6c6e91c42a927271544695e664b3263ab18a5
a112kk <imamsafaraliev11@gmail.com>
2023-02-08 18:56:23 +0300

    ToastProject

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.myapplication
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.graphics.component1
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,21 +14,17 @@ import androidx.recyclerview.widget.RecyclerView
 import java.util.Scanner
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var  viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
+    @SuppressLint("MissingInflatedId", "ShowToast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewManager = LinearLayoutManager(this)
-        viewAdapter = MyAdapter(listOf("item1","item2","item3"))
-
-        recyclerView = findViewById<RecyclerView>(R.id.recyclerView).
-        apply {
-            setHasFixedSize(true)
-
-            layoutManager = viewManager
-            adapter = viewAdapter
+        var button: Button = findViewById(R.id.button2)
+        button.setOnClickListener {
+            val text = "Успешно!"
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, text, duration,)
+            toast.setGravity(Gravity.CENTER,0,0)
+            toast.show()
         }
 
     }
